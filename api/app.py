b6c608c6568f561c29e165cc317296dc8c9ba3a3
Samantha Orizabal <sam.orizabal@gmail.com>
2021-01-16 20:35:20 -0500

    Fix db models and db

--- api/app.py
+++ api/app.py
@@ -9,8 +9,8 @@ db = SQLAlchemy(app)
 
 
 task_tag_table = db.Table('task_tag',
-    db.Column('task_id', db.Integer, db.ForeignKey('task.task_id'), primary_key=True),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.tag_id'), primary_key=True)
+    db.Column('task_id', db.Integer, db.ForeignKey('task.task_id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.tag_id'))
 )
 
 
@@ -21,7 +21,7 @@ class task(db.Model):
     task_content = db.Column(db.String(500), nullable=False)
     date_start = db.Column(db.DateTime, default=datetime.utcnow)
     date_end = db.Column(db.DateTime)
-    tags = db.relationship("tag", secondary=task_tag_table)
+    tags = db.relationship("tag", secondary=task_tag_table, backref=db.backref('tasks_associated', lazy='joined'))
 
     def __repr__(self):
         return '<Task %r>' % self.task_id
@@ -32,7 +32,6 @@ class tag(db.Model):
     tag_id = db.Column(db.Integer, nullable=False, primary_key=True)
     tag_title = db.Column(db.String(50), nullable=False)
     tag_desc = db.Column(db.String(500))
-    tasks = db.relationship("task", secondary=task_tag_table)
 
     def __repr__(self):
         return '<Tag %r>' % self.tag_id
@@ -53,12 +52,23 @@ def index():
         "Todo" : "List"
     }
 
+
 @app.route('/_get_date', methods=['GET'])
 def get_date():
     local_dt = get_timezone()
     return jsonify({
         'date' : local_dt.strftime("%A, %B %d")
     })
+
+
+@app.route('/create_task', methods=['POST'])
+def create_task():
+    task_title = request.form['title'] # Task title
+    task_content = request.form['content'] # Task content
+    date_end = request.form['date_end'] # Task due date
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
